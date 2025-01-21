8b5c0cb90758d358f2cae1dbe65aaee99710ea53
s0up4200 <s0up4200@pm.me>
2025-01-21 09:17:58 +0100

    chore(deps): update go-humanize to v1.0.1 and go-colorable to v0.1.14

--- internal/torrent/hasher.go
+++ internal/torrent/hasher.go
@@ -278,3 +278,27 @@ func NewPieceHasher(files []fileEntry, pieceLen int64, numPieces int, display Di
 		display:   display,
 	}
 }
+
+// minInt returns the smaller of two integers
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// minInt64 returns the smaller of two int64 values
+func minInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// maxInt64 returns the larger of two int64 values
+func maxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
