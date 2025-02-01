2af27cbdcdc9e0ccb818cbe13fd08dd08c148404
ze0s <ze0s@riseup.net>
2025-02-01 13:56:45 +0100

    feat: add self-update command

---
+++ cmd/update.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+	"github.com/spf13/cobra"
+)
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update mkbrr",
+	RunE:  runUpdate,
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
+
+func runUpdate(cmd *cobra.Command, args []string) error {
+	v, err := semver.ParseTolerant(version)
+	if err != nil {
+		return fmt.Errorf("could not parse version: %w", err)
+	}
+
+	latest, err := selfupdate.UpdateSelf(v, "autobrr/mkbrr")
+	if err != nil {
+		return fmt.Errorf("could not selfupdate: %w", err)
+	}
+
+	if latest.Version.Equals(v) {
+		// latest version is the same as current version. It means current binary is up-to-date.
+		fmt.Printf("Current binary is the latest version: %s\n", version)
+	} else {
+		fmt.Printf("Successfully updated to version: %s\n", latest.Version)
+	}
+	return nil
+}
