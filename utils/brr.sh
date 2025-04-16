9fd3014471d80d4081dc521e07a92dce357a5326
s0up4200 <s0up4200@pm.me>
2025-04-16 19:20:35 +0200

    fix(benchmark): file path input in brr.sh script

--- utils/brr.sh
+++ utils/brr.sh
@@ -2,14 +2,22 @@
 
 # Script to benchmark mkbrr with different number of workers
 
-FILE_PATH="/path/to/source/data"
+if [ $# -eq 0 ]; then
+    echo "Error: No file path provided"
+    echo "Usage: $0 <file_path>"
+    exit 1
+fi
 
+FILE_PATH="$1"
+
+if [ ! -d "$FILE_PATH" ]; then
+    echo "Error: Directory '$FILE_PATH' does not exist"
+    exit 1
+fi
 
 WORKER_COUNTS=(1 2 3 4 5 6 7 8)
 
-
 COMMANDS=("hyperfine" "--warmup" "1" "--runs" "5")
-
 
 for WORKERS in "${WORKER_COUNTS[@]}"; do
   COMMAND="mkbrr create '$FILE_PATH' --workers $WORKERS"
